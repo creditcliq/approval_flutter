f97552da1b92f974baa03d1794ed897e3ab9f7be
Blue-Marvel <marvellousogbo0@gmail.com>
2026-09-15 21:06:59 +0100

    build: remove mavenLocal() to strictly resolve from JitPack

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,7 +19,6 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        mavenLocal() // <--- Add mavenLocal() remove it from prod
         maven { url = uri("https://jitpack.io") }
     }
 }
